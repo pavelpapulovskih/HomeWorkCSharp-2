22d172cbb307e773d229be7b8c174db9d71c11a7
Your Pavel <p9l-real-chel@mail.ru>
2023-03-11 01:07:47 +0500

    задача11

---
+++ Задача11/Program.cs
@@ -0,0 +1,9 @@
+﻿// задача 11 hard. Напишите программу, которая принимает от пользователя целое число любой разрядности и удаляет вторую цифру этого числа.
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int allNum = (int)Math.Log10(num) + 1; 
+int predLastNumMnojitel = (int)Math.Pow(10, allNum - 2); 
+int result = num % predLastNumMnojitel + num / predLastNumMnojitel / 10 * predLastNumMnojitel; 
+Console.WriteLine(result); 
